10b94c0c9f5bdc39449b71f59f6c4dd1761d377a
Ivona <ivonamilanova221@gmail.com>
2017-07-28 18:43:52 +0200

    factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -24,13 +24,3 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     ];
 });
 
-$factory->define(TodoRepository::class, function (Faker\Generator $faker) {
-    static $password;
-
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
-    ];
-});
