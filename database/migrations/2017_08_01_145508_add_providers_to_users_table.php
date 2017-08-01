33bd6f8bfe8497e75576db9a5db962ca6d9954d3
Ivona <ivonamilanova221@gmail.com>
2017-08-01 11:45:59 -0400

    testDoneAndSocialLogin2

---
+++ database/migrations/2017_08_01_145508_add_providers_to_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProvidersToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table) {
+            $table->string('email')->nullable()->change();
+            $table->string('password', 60)->nullable()->change();
+            $table->string('provider')->nullable();
+            $table->string('provider_id')->nullable();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function($table) {
+            $table->dropColumn('providers');
+            $table->dropColumn('provider_id');
+
+
+
+        });
+    }
+}
